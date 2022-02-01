e72369b9ea9d3ba1e38d9cfa73e75cfa9f1bbd4c
Alina Buzachis <abuzachis@redhat.com>
2022-02-01 16:52:39 +0100

    fix parameters()
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- amazon_cloud_code_generator/cmd/refresh_modules.py
+++ amazon_cloud_code_generator/cmd/refresh_modules.py
@@ -221,25 +221,9 @@ class AnsibleModule(AnsibleModuleBase):
         super().__init__(resource, definitions)
         # TODO: We can probably do better
         self.default_operationIds = []
-
-
-class AnsibleInfoModule(AnsibleModuleBase):
-    def __init__(self, resource, definitions):
-        super().__init__(resource, definitions)
-        self.name = resource.name + "_info"
-        self.default_operationIds = ["get", "list"]
-
+    
     def parameters(self):
         return [i for i in list(super().parameters()) if i["name"] != "state"]
-
-
-class AnsibleInfoNoListModule(AnsibleInfoModule):
-    template_file = "info_no_list_module.j2"
-
-
-class AnsibleInfoListOnlyModule(AnsibleInfoModule):
-    template_file = "info_list_and_get_module.j2"
-
 
 
 class Definitions:
