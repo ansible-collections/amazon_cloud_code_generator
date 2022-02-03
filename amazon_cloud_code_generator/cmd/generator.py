d4322dfdd6fa8a5bed57b770b49a28324eb599cc
Alina Buzachis <abuzachis@redhat.com>
2022-02-03 15:04:11 +0100

    Cleanuo generator.py
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- amazon_cloud_code_generator/cmd/generator.py
+++ amazon_cloud_code_generator/cmd/generator.py
@@ -10,40 +10,39 @@ import yaml
 import pkg_resources
 
 
-resources = [
-    "AWS::EC2::SecurityGroup",
-    "AWS::CloudWatch::Alarm",
-    "AWS::EC2::Instance",
-    "AWS::AutoScaling::AutoScalingGroup",
-    "AWS::IAM::Policy",
-    "AWS::IAM::InstanceProfile",
-    "AWS::ElasticLoadBalancingV2::TargetGroup",
-    "AWS::AutoScaling::ScalingPolicy",
-    "AWS::EC2::SecurityGroupIngress",
-    "AWS::EC2::VPCGatewayAttachment",
-    "AWS::S3::BucketPolicy",
-    "AWS::ElasticLoadBalancingV2::LoadBalancer",
-    "AWS::ApiGateway::Deployment",
-    "AWS::ApiGateway::RestApi",
-    "AWS::SSM::Parameter",
-    "AWS::EC2::SecurityGroupEgress",
-    "AWS::RDS::DBSubnetGroup",
-    "AWS::SecretsManager::Secret",
-    "AWS::EC2::Volume",
-    "AWS::RDS::DBCluster",
-    "AWS::EKS::Nodegroup",
-    "AWS::RDS::DBClusterParameterGroup",
-    "AWS::EC2::VPCCidrBlock",
-    "AWS::EC2::NetworkInterfaceAttachment",
-    "AWS::Route53Resolver::ResolverRuleAssociation",
-    "AWS::EC2::SubnetCidrBlock",
+RESOURCES = [
+    "AWS::S3::Bucket",
+    #"AWS::EC2::SecurityGroup",
+    # "AWS::CloudWatch::Alarm",
+    # "AWS::EC2::Instance",
+    # "AWS::AutoScaling::AutoScalingGroup",
+    # "AWS::IAM::Policy",
+    # "AWS::IAM::InstanceProfile",
+    # "AWS::ElasticLoadBalancingV2::TargetGroup",
+    # "AWS::AutoScaling::ScalingPolicy",
+    # "AWS::EC2::SecurityGroupIngress",
+    # "AWS::EC2::VPCGatewayAttachment",
+    # "AWS::S3::BucketPolicy",
+    # "AWS::ElasticLoadBalancingV2::LoadBalancer",
+    # "AWS::ApiGateway::Deployment",
+    # "AWS::ApiGateway::RestApi",
+    # "AWS::SSM::Parameter",
+    # "AWS::EC2::SecurityGroupEgress",
+    # "AWS::RDS::DBSubnetGroup",
+    # "AWS::SecretsManager::Secret",
+    # "AWS::EC2::Volume",
+    # "AWS::RDS::DBCluster",
+    # "AWS::EKS::Nodegroup",
+    # "AWS::RDS::DBClusterParameterGroup",
+    # "AWS::EC2::VPCCidrBlock",
+    # "AWS::EC2::NetworkInterfaceAttachment",
+    # "AWS::Route53Resolver::ResolverRuleAssociation",
+    # "AWS::EC2::SubnetCidrBlock",
 ]
 
-resources_test = ["AWS::S3::Bucket"] #, "AWS::EC2::Instance", "AWS::EC2::Volume"]
 MODULE_NAME_MAPPING = {
     "AWS::S3::Bucket": "s3_bucket",
 }
-
 
 def python_type(value):
     TYPE_MAPPING = {
@@ -56,58 +55,56 @@ def python_type(value):
     return TYPE_MAPPING.get(value, value)
 
 
-def preprocess(a_dict, k, subst_dict):
+def preprocess(a_dict, subst_dict):
     a_dict_copy = copy.copy(a_dict)
     
     for key in a_dict_copy.keys():
         if isinstance(a_dict[key], list):            
             if key == "enum":
                 a_dict["choices"] = sorted(a_dict.pop(key))
-    
         elif isinstance(a_dict_copy[key], dict):
-            if  "$ref" in a_dict_copy[key]:
-                lookup_param = a_dict_copy[key]['$ref'].split('/')[-1].strip()
+            if key == "properties":
+                a_dict["suboptions"] = a_dict.pop(key)
+                key = "suboptions"
+            if "items" in a_dict[key]:
+                a_dict[key] = dict(a_dict_copy[key], **a_dict.pop(key))
+            if  "$ref" in a_dict[key]:
+                lookup_param = a_dict[key]['$ref'].split('/')[-1].strip()
                 for _, _ in subst_dict.items():
                     if subst_dict.get(lookup_param):
                         a_dict[key] = subst_dict[lookup_param]
                     break
             else:
-                preprocess(a_dict[key], k, subst_dict)
-        else:
+                preprocess(a_dict[key], subst_dict)
+        elif isinstance(a_dict[key], str): 
             if key == "type":
                 a_dict[key] = python_type(a_dict_copy[key])
             if key == "description":
-                a_dict[key] = [a_dict_copy[key]]
+                a_dict[key] = a_dict_copy[key]
             if key == "const":
                 a_dict["defaults"] = a_dict.pop(key)
 
 
-def cleanup(a_dict):
+def ensure_required(a_dict):
     a_dict_copy = copy.copy(a_dict)
 
     if not isinstance(a_dict, dict):
         return a_dict
     
-    for k, v in a_dict_copy.items():        
-        if k == "properties":
-            a_dict["suboptions"] = a_dict.pop(k)
-    
+    for k, v in a_dict_copy.items():
         if isinstance(v, dict) and "required" in v and isinstance(v["required"], list):
             for r in v["required"]:
-                if "properties" in a_dict[k]:
-                    a_dict[k]["properties"][r]["required"] = True
-                else:
-                    a_dict[k]["suboptions"][r]["required"] = True
+                a_dict[k]["suboptions"][r]["required"] = True
             a_dict[k].pop("required")
 
-        cleanup(a_dict_copy[k])
+        ensure_required(a_dict_copy[k])
 
 
-def filter(a_dict):
+def cleanup_keys(a_dict):
     list_of_keys_to_remove = ["additionalProperties", "insertionOrder", "uniqueItems"]
     if not isinstance(a_dict, dict):
         return a_dict
-    return {k: v for k, v in ((k, filter(v)) for k, v in a_dict.items()) if k not in list_of_keys_to_remove}
+    return {k: v for k, v in ((k, cleanup_keys(v)) for k, v in a_dict.items()) if k not in list_of_keys_to_remove}
 
 
 def _camel_to_snake(name, reversible=False):
@@ -136,11 +133,11 @@ def _camel_to_snake(name, reversible=False):
     return re.sub(all_cap_pattern, r'\1_\2', s2).lower()
 
 
-def camel_to_snanke(a_dict):
+def camel_to_snake(a_dict):
     b_dict = {}
     for k in a_dict.keys():
         if isinstance(a_dict[k], dict):
-            b_dict[_camel_to_snake(k)] = camel_to_snanke(a_dict[k])
+            b_dict[_camel_to_snake(k)] = camel_to_snake(a_dict[k])
         else:
             b_dict[_camel_to_snake(k)] = a_dict[k]
     return b_dict
@@ -156,29 +153,30 @@ def get_module_from_config(module):
     return False
 
 
-def generate_documentation(module_name, description, definitions, added_ins, next_version):
-    description = [description]
+def generate_documentation(module, added_ins, next_version):
+    module_name = module.name 
+    definitions = module.definitions.definitions
+    description = module.description
     documentation = {
         "module": module_name,
-        "author": ["Ansible Cloud Team (@ansible-collections)"],
-        "description": [description],
-        "short_description": [description],
+        "author": "Ansible Cloud Team (@ansible-collections)",
+        "description": description,
+        "short_description": description,
         "options": definitions,
         "requirements": [],
         "version_added": added_ins["module"] or next_version,
     }
 
-    preprocess(documentation, "$ref", documentation["options"])
-    cleanup(documentation["options"])
-
-    documentation = filter(documentation)
-    documentation = camel_to_snanke(documentation)
+    preprocess(documentation, documentation["options"])
+    ensure_required(documentation["options"])
+    documentation = cleanup_keys(documentation)
+    documentation = camel_to_snake(documentation)
 
     module_from_config = get_module_from_config(module_name)
     if module_from_config and "documentation" in module_from_config:
         for k, v in module_from_config["documentation"].items():
             documentation[k] = v
-    
+
     return documentation
 
 
@@ -199,4 +197,5 @@ class CloudFormationWrapper:
         """
         # TODO: include version
         response = self.client.describe_type(Type='RESOURCE', TypeName=type_name)
+
         return response.get('Schema')
