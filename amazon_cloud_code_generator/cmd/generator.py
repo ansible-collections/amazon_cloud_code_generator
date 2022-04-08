92ae9c1daf9a8af8f4157fdf8def1315ae84d901
Alina Buzachis <abuzachis@redhat.com>
2022-04-08 17:41:07 +0200

    Tighten the list of supported resources by removing those with multiple identifiers (#22)
    Tighten the list of supported resources by removing those with multiple identifiers
    
    SUMMARY
    
    Tighten the list of supported resources by removing those with multiple identifiers
    
    ISSUE TYPE
    
    
    Bugfix Pull Request
    Docs Pull Request
    Feature Pull Request
    New Module Pull Request
    
    COMPONENT NAME
    
    ADDITIONAL INFORMATION
    
    Reviewed-by: Mike Graves <mgraves@redhat.com>

--- amazon_cloud_code_generator/cmd/generator.py
+++ amazon_cloud_code_generator/cmd/generator.py
@@ -48,6 +48,7 @@ class Description:
                 "DDThh",
                 "ARNs",
                 "VPCs",
+                "AWS::EFS::MountTarget",
             ]
         )
         ignored_values = set(["PUT", "S3"])
@@ -151,6 +152,18 @@ class Documentation:
                     options["choices"] = sorted(options.pop(key))
                 if key == "type":
                     options[key] = python_type(options[key])
+                if key == "oneOf":
+                    one_of = options.pop(key)
+                    to_be_updated: Dict = {}
+                    for elem in one_of:
+                        if "required" in elem:
+                            elem.pop("required")
+                        if "properties" in elem:
+                            to_be_updated.update(elem["properties"])
+                        else:
+                            to_be_updated.update(elem)
+                    options["suboptions"] = to_be_updated
+                    self.replace_keys(options["suboptions"], definitions)
             elif isinstance(item, dict):
                 if key == "properties":
                     options["suboptions"] = options.pop(key)
@@ -209,9 +222,11 @@ class Documentation:
 
                 if "required" in v and isinstance(v["required"], list):
                     for r in v["required"]:
-                        if "default" in a_dict[k]["suboptions"][r]:
+
+                        if "default" in a_dict[k]["suboptions"]:
                             continue
-                        a_dict[k]["suboptions"][r]["required"] = True
+                        if r in a_dict[k]["suboptions"]:
+                            a_dict[k]["suboptions"][r]["required"] = True
                     a_dict[k].pop("required")
 
             self.ensure_required(a_dict[k])
@@ -228,6 +243,9 @@ class Documentation:
             "format",
             "minimum",
             "maximum",
+            "patternProperties",
+            "maxItems",
+            "minItems",
         ]
         self.replace_keys(self.options, self.definitions)
         self.ensure_required(self.options)
@@ -276,16 +294,16 @@ def generate_documentation(
     }
 
     docs = Documentation()
-    docs.options = module.schema.get("properties")
-    docs.definitions = module.schema.get("definitions")
+    docs.options = module.schema.get("properties", {})
+    docs.definitions = module.schema.get("definitions", {})
 
     # Properties defined as required must be specified in the desired state during resource creation
-    docs.required = module.schema.get("required")
+    docs.required = module.schema.get("required", [])
 
     # Properties defined as readOnlyProperties can't be set by users
-    docs.read_only_properties = module.schema.get("readOnlyProperties")
+    docs.read_only_properties = module.schema.get("readOnlyProperties", [])
 
-    docs.primary_identifier = module.schema.get("primaryIdentifier")
+    docs.primary_identifier = module.schema.get("primaryIdentifier", [])
 
     # Properties defined as writeOnlyProperties can be specified by users when creating or updating a
     # resource but can't be returned during a read or list requested
